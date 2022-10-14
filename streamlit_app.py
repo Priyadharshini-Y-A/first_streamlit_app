d4cee62cef6cc62792f08d79395792bb56a4a110
Priyadharshini-Y-A <115782792+Priyadharshini-Y-A@users.noreply.github.com>
2022-10-14 20:59:40 +0530

    Update streamlit_app.py
    setting fruit column as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,5 @@ streamlit.dataframe(my_fruit_list)
 
 #Let's put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick Some fruits:",list(my_fruit_list.index))
+
+my_fruit_list=my_fruit_list.(index('Fruit'))
