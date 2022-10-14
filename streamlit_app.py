7bd3c6461d41943cef70368bf717cedef2660ec8
Priyadharshini-Y-A <115782792+Priyadharshini-Y-A@users.noreply.github.com>
2022-10-14 18:30:57 +0530

    Update streamlit_app.py
    imported pandas
    pulled a data set into pandas data frame
    and displayed it

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.text('Rice')
 streamlit.text('Saambar')
 streamlit.text('Potato Fry')
 streamlit.text('Curd Rice')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe('my_fruit_list')
