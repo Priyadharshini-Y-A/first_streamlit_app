b5bede0fbdfef9ac97470898f583b19ccd6e6671
Priyadharshini-Y-A <115782792+Priyadharshini-Y-A@users.noreply.github.com>
2022-10-28 16:13:00 +0530

    Update streamlit_app.py
    Adding Another section to display Fruitvice API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,17 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#Another section to display Fruitvice API response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.text(fruityvice_response.json())--just write the data to screen
+
+# Normalize the json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output in table format
+streamlit.dataframe(fruityvice_normalized)
